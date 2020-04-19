152af66ecc8eddd62f71f978fea82ffd3d96e7f3
Devashish Rajput <42275894+Devashish-Rajput@users.noreply.github.com>
2020-04-19 20:03:37 +0530

    settings page is completed

--- MessengerApp/app/src/main/java/com/example/messengerapp/Fragments/SettingsFragment.kt
+++ MessengerApp/app/src/main/java/com/example/messengerapp/Fragments/SettingsFragment.kt
@@ -1,16 +1,30 @@
 package com.example.messengerapp.Fragments
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.ProgressDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import com.example.messengerapp.ModelClasses.Users
 
 import com.example.messengerapp.R
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
@@ -21,6 +35,11 @@ class SettingsFragment : Fragment() {
 
     var usersReference: DatabaseReference?=null
     var firebaseUser: FirebaseUser?=null
+    private var RequestCode=438
+    private var imageUri:Uri?=null
+    private var storageRef: StorageReference?=null
+    private var coverChecker:String?=""
+    private var socialChecker:String?=""
 
 
     override fun onCreateView(
@@ -32,6 +51,7 @@ class SettingsFragment : Fragment() {
 
         firebaseUser=FirebaseAuth.getInstance().currentUser
         usersReference=FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        storageRef=FirebaseStorage.getInstance().reference.child("User Images")
 
         usersReference!!.addValueEventListener(object :ValueEventListener{
             override fun onDataChange(p0: DataSnapshot) {
@@ -52,8 +72,162 @@ class SettingsFragment : Fragment() {
             }
         })
 
+        view.profile_image_settings.setOnClickListener{
+            pickImage()
+        }
+
+        view.cover_image_settings.setOnClickListener{
+            coverChecker="cover"
+            pickImage()
+        }
+
+        view.set_facebook.setOnClickListener{
+            socialChecker="facebook"
+            setSocialLinks()
+        }
+        view.set_instagram.setOnClickListener{
+            socialChecker="instagram"
+            setSocialLinks()
+        }
+        view.set_website.setOnClickListener{
+            socialChecker="website"
+            setSocialLinks()
+        }
 
         return view
+    }
+
+    private fun setSocialLinks() {
+        val builder:AlertDialog.Builder=
+            AlertDialog.Builder(context!!, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+
+        if (socialChecker=="website")
+        {
+            builder.setTitle("Write Url:")
+        }
+        else{
+            builder.setTitle("Write Username")
+        }
+
+        val editText= EditText(context)
+
+        if (socialChecker=="website")
+        {
+            editText.hint="e.g www.google.com"
+        }
+        else{
+            editText.hint="e.g sr6883"
+        }
+        builder.setView(editText)
+
+        builder.setPositiveButton("Create",DialogInterface.OnClickListener {
+                dialog, which ->
+            val str=editText.text.toString()
+
+            if(str=="") {
+                Toast.makeText(context,"Please Write Something...", Toast.LENGTH_LONG).show()
+            } else {
+                saveSocial(str)
+            }
+        })
+        builder.setNegativeButton("Cancel",DialogInterface.OnClickListener{
+                dialog, which ->
+            dialog.cancel()
+
+        })
+
+        builder.show()
+
+    }
+
+    private fun saveSocial(str: String) {
+        val mapSocial=HashMap<String, Any>()
+
+        when(socialChecker)
+        {
+            "facebook"->
+            {
+                mapSocial["facebook"]="https://m.facebook.com/$str"
+            }
+            "instagram"->
+            {
+                mapSocial["instagram"]="https://m.facebook.com/$str"
+            }
+            "instagram"->
+            {
+                mapSocial["instagram"]="https://$str"
+            }
+        }
+         usersReference!!.updateChildren(mapSocial).addOnCompleteListener {
+             task ->
+             if(task.isSuccessful)
+             {
+                 Toast.makeText(context,"Updated Successfully", Toast.LENGTH_LONG).show()
+             }
+         }
+    }
+
+    private fun pickImage() {
+        val intent= Intent()
+        intent.type="image/*"
+        intent.action=Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, RequestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==RequestCode&& resultCode==Activity.RESULT_OK && data!!.data!=null)
+        {
+            imageUri=data.data
+            Toast.makeText(context,"uploading...", Toast.LENGTH_LONG).show()
+            uploadImageToDatabase()
+        }
+    }
+
+    private fun uploadImageToDatabase() {
+        val progressBar= ProgressDialog(context)
+        progressBar.setMessage("image is uploading, please wait...")
+        progressBar.show()
+        if (imageUri!=null)
+        {
+            val fileRef=storageRef!!.child(System.currentTimeMillis().toString()+".jpg")
+
+            var uploadTask: StorageTask<*>
+            uploadTask=fileRef.putFile(imageUri!!)
+
+            uploadTask.continueWithTask(Continuation <UploadTask.TaskSnapshot,Task<Uri>>{ task->
+                if(!task.isSuccessful)
+                {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation fileRef.downloadUrl
+            }).addOnCompleteListener { task->
+                if(task.isSuccessful)
+                {
+                    val downloadUrl=task.result
+                    val url=downloadUrl.toString()
+
+                    if (coverChecker=="cover")
+                    {
+                        val mapCoverImg=HashMap<String, Any>()
+                        mapCoverImg["cover"]=url
+                        usersReference!!.updateChildren(mapCoverImg)
+                        coverChecker=""
+                    }
+                    else
+                    {
+                        val mapProfileImg=HashMap<String, Any>()
+                        mapProfileImg["profile"]=url
+                        usersReference!!.updateChildren(mapProfileImg)
+                        coverChecker=""
+                    }
+                    progressBar.dismiss()
+                }
+            }
+        }
     }
 
 }
